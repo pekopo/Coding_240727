8cf7247555efb4c26c406ab83f13763fbc636f23
pokope <152680794+pekopo@users.noreply.github.com>
2024-07-27 15:11:07 +0900

    sato turn2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,19 +4,23 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
-		List<String> answerlist = new ArrayList<String>();
+		List<String> answerlist = new ArrayList<>();
 		String question = "赤いフルーツを選んでください";
-		
+
 		answerlist.add("バナナ");
 		answerlist.add("イチゴ");
 		answerlist.add("メロン");
-		
-		Map<Integer, List<String>> keyToChoice = new HashMap<Integer, List<String>>();
-		
+
+		Map<Integer, List<String>> keyToChoice = new HashMap<>();
+		Random r = new Random();
+		int answerNumber = r.nextInt(2);
+
+		System.out.println(String.format("問題文: %s", question));
 	}
 
 }
